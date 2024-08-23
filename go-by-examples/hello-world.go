34fe9c53b01bf1dcb7872aacef875f68d9bc4980
Lucas Schwalm Silva <lnaoedelixo42@gmail.com>
2024-08-23 09:55:54 -0300

    Methods and structs

--- go-by-examples/hello-world.go
+++ go-by-examples/hello-world.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+type fruit struct {
+	name string
+	qtd  int
+}
+
+func (f *fruit) buy() {
+	f.qtd++
+}
+
 func main() {
 
 	for a := range 3 {
@@ -24,4 +33,11 @@ func main() {
 	compras[2] = "beteraba"
 	new_sliced := compras[1:3] // includes 1, don't includes 3, just like the default loop
 	fmt.Println(new_sliced)
+
+	// slice is basically a linked list in the end of the day
+	bying_list := []fruit{}
+	// reappend the pointer to new slice returned by append()
+	bying_list = append(bying_list, fruit{name: "Banana"})
+	bying_list[0].buy() // buys another banana
+	fmt.Println("I have", bying_list[0].qtd, "bananas")
 }
