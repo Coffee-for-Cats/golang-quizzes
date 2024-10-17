fc44117fe4214a670fe2e8f6aaf9d6a0d183cccd
Lucas Schwalm Silva <lnaoedelixo42@gmail.com>
2024-10-17 13:56:35 -0300

    routes rearrangement

--- main.go
+++ main.go
@@ -7,15 +7,13 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/register", routes.Register)
-
 	http.HandleFunc("GET /quiz/random", routes.GetRandomQuiz)
 	http.HandleFunc("GET  /quiz/{quizID}/", routes.GetQuiz)
-	http.HandleFunc("POST /question", routes.PostQuestion)
-	http.HandleFunc("POST /quiz", routes.PostQuiz)
-	http.HandleFunc("POST /answer/{questionID}/", routes.PostResponse)
 
-	// http.HandleFunc("/quiz/random/", routes.GetRandomQuiz)
+	http.HandleFunc("POST /register", routes.Register)
+	http.HandleFunc("POST /quiz", routes.PostQuiz)
+	http.HandleFunc("POST /question", routes.PostQuestion)
+	http.HandleFunc("POST /answer/{questionID}/", routes.PostResponse)
 
 	fmt.Println("🎉 Sever up! (on :8080) 🎉")
 	http.ListenAndServe(":8080", nil)
